f8a293152c106510d746d8c95b321a5373be4471
Vrajesh Bhatt <vrajbhatt005@gmail.com>
2025-12-01 18:44:25 -0500

    added potfoliodata file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { portfolioData } from './portfolioData';
 import { motion, AnimatePresence } from 'framer-motion';
 import emailjs from '@emailjs/browser';
 import { 
